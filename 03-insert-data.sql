a6ed845d6e380bbf10c953c3e0f6af947eabb74f
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-24 01:30:41 +0000

    feat: inserting related data

---
+++ 03-insert-data.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO cities(name) VALUES('Berlin'),('New York'),('London');
+-- INSERT INTO address (street, house_number, city_id)
+-- VALUES('TestStreet', '8A', 3),
+--     ('Some Street', '10', 1),
+--     ('TestStreet', '1', 3),
+--     ('My street', '101', 2);
+-- INSERT INTO users(first_name, last_name, email, address_id)
+-- VALUES ('Burhanudin', 'Rabbani', 'bani@test.io', 2),
+--     ('Manual', 'Lorenz', 'manual@test.io', 4),
+--     ('Julie', 'Bernes', 'Julie@test.io', 3);
